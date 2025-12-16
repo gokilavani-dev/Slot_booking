e20044c7273330e7ff15af53af78e15ccab00918
gokilavani-dev <gokilavani2k@gmail.com>
2025-12-16 16:20:23 +0530

    Creating dealer problem fixed

--- Backend/src/models/user.model.js
+++ Backend/src/models/user.model.js
@@ -4,7 +4,7 @@ const userSchema = new mongoose.Schema(
   {
     email: { type: String, required: true, unique: true, lowercase: true, trim: true },
     password: { type: String, required: true },
-    role: { type: String, enum: ["ADMIN", "DEALER"], default: "dealer" }
+    role: { type: String, enum: ["ADMIN", "DEALER"], default: "DEALER" }
   },
   { timestamps: true }
 );
